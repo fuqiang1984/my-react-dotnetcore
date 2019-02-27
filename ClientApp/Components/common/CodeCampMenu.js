586c797140b7fc784d2362768dfcaf7792db6f6b
unknown <547557356@qq.com>
2019-02-27 17:41:45 +1300

    remove header for survey

--- ClientApp/Components/common/CodeCampMenu.js
+++ ClientApp/Components/common/CodeCampMenu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function CodeCampMenu() {
+    
     return (
         <div>
             <div className="header__open-button-mobile">
