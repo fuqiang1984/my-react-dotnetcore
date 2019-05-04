a162305f554b9e696b5a76a0baa18ffe0f253ac9
杜雪莲 <john.fu1984@gmail.com>
2019-05-04 23:40:51 +1200

    add sidebar

--- ClientApp/Components/Layout/MainLayout.js
+++ ClientApp/Components/Layout/MainLayout.js
@@ -1,16 +1,14 @@
 import React from 'react';
+import { Sidebar } from '../../Components/Layout';
 
 class MainLayout extends React.Component {
   
 
   render() {
-    console.log('123');
+    //console.log('123');
     const { children } = this.props;
-    console.log(children);
-    return (
-      <main className="cr-app bg-light">
-         {children}
-      </main>
+    //console.log(children);
+    return (<main className="cr-app bg-light"><React.Fragment><Sidebar></Sidebar>{children}</React.Fragment></main>
     );
   }
 }
--- ClientApp/Components/Layout/index.js
+++ ClientApp/Components/Layout/index.js
@@ -1,4 +1,5 @@
 
 export { default as LayoutRoute } from './LayoutRoute';
 export { default as MainLayout } from './MainLayout';
+export { default as Sidebar } from './Sidebar';
 
